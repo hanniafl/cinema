504aa2a1d74b852369a6fe21001b4a6578fe8500
Hannia Flores <hannia.flores2958@alumnos.udg.mx>
2021-05-17 18:25:29 -0500

    agregamos modelo y migraciones

---
+++ database/migrations/2021_05_17_181814_create_pelicula_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeliculaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pelicula', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('director');
+            $table->smallInteger('valoracion');
+            $table->text('resena');
+            $table->date('fecha_visto');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pelicula');
+    }
+}
